e89ffd127a87e0da181af8bbc5ed84e3e7531451
gabystern <stern.gabrielle@gmail.com>
2017-06-15 17:59:57 -0400

    joiner serlializer woooo

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -4,7 +4,6 @@ module Api
 
       def index
         render json: Topic.all
-        # render json: @topics, include: ['title_medium', 'title_text', 'events.event_medium', 'events.event_medium.url', 'events.event_medium.caption', 'events.event_text', 'events.event_text.headline', 'events.event_text.text']
       end
 
       def show
