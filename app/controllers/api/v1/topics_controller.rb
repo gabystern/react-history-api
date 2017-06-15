8caa6f234ae9d3deea7457932b32444936090cf9
gabystern <stern.gabrielle@gmail.com>
2017-06-15 17:58:38 -0400

    fixed serializer

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -3,9 +3,8 @@ module Api
     class TopicsController < ApplicationController
 
       def index
-        @topics = Topic.all
-
-        render json: @topics, include: ['title_medium', 'title_text', 'events.event_medium', 'events.event_medium.url', 'events.event_medium.caption', 'events.event_text', 'events.event_text.headline', 'events.event_text.text']
+        render json: Topic.all
+        # render json: @topics, include: ['title_medium', 'title_text', 'events.event_medium', 'events.event_medium.url', 'events.event_medium.caption', 'events.event_text', 'events.event_text.headline', 'events.event_text.text']
       end
 
       def show
